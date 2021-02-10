0fa1a921e5c60884cef9032fce9bda888f5aa36f
Susan <cunohildebrand@gmail.com>
2021-02-10 10:33:29 -0700

    Revert "Update login.js"
    This reverts commit fa4df54a56606aa0ad503326bc969d551f455dfe.

--- src/components/login.js
+++ src/components/login.js
@@ -1,54 +1,13 @@
 import React from 'react';
-import { API } from '../api';
-import { Redirect } from "react-router-dom";
 
 export class Login extends React.Component {
-
-    constructor(props){
-        super(props)
-        this.state = { redirect : '' }
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    
-    componentDidMount()
-    {    
-        let token = localStorage.getItem('token')
-        if (token != null) {
-            // if there is a token and token is valid for the user,
-            // redirect, else do nothing and let the user login
-            API.get_current_user(token).then((response) => {
-                this.setState({
-                    redirect : "/"
-                })
-            })
-        }
-    }
-
-    handleSubmit(event)
-    {
-        event.preventDefault()
-        let username = event.target.username.value
-        let password = event.target.password.value
-        API.login(username, password).then((response) => {
-            localStorage.setItem("token", response.data.token)
-            this.setState({
-                redirect : "/"
-            })
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
-
     render() {
-        if (this.state.redirect) {
-            return <Redirect to={this.state.redirect} />
-        }
         return (
-            <div className="content_container">
+            <div className="content-container">
                 <h1>Customer Login</h1>
-                <form onSubmit={this.handleSubmit} method="POST">
+                <form action="none">
                     <input type="text" placeholder="Username" name="username" />
-                    <input type="password" placeholder="Password" name="password" />
+                    <input type="password" placeholder="Password" name="psw" />
                     <button type="submit">Login</button>
                 </form>
             </div>
